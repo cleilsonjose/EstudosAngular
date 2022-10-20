3c2cf1e9d64404f5154fd8a4c1c3b190a2e19c2b
cleilsonjose <cleilsonrock@gmail.com>
2022-10-20 11:21:18 -0300

    ngStyle

--- meu-projeto-1/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ meu-projeto-1/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.scss']
 })
 export class DiretivasAtributosComponent implements OnInit {
-  public valor:boolean=true;
+  public valor: boolean = true;
+  public heightPx: string = "120px";
+  public backgroundColor:string = "#1e1e1e";
+
+
   constructor() {
     //
   }
 
   ngOnInit(): void {
-    //
+    setInterval(()=>{
+      if(this.heightPx == "120px"){
+        this.heightPx = "150px";
+        this.backgroundColor = "#e1e1e1";
+      }else{
+        this.heightPx = "120px";
+        this.backgroundColor = "#751d1d";
+      }
+
+    },1000)
   }
+
   public dark(){
     if(this.valor){
       this.valor = false;

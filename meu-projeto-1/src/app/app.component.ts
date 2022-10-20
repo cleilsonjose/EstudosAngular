edeebf9aa646671accd142e10944c616f730d396
cleilsonjose <cleilsonrock@gmail.com>
2022-10-20 14:30:16 -0300

    ngModel

--- meu-projeto-1/src/app/app.component.ts
+++ meu-projeto-1/src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <!--
     <app-title></app-title>
     <app-data-biding></app-data-biding>
     <app-diretivas-estruturais></app-diretivas-estruturais>
+  -->
     <app-diretivas-atributos></app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
--- meu-projeto-1/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ meu-projeto-1/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.scss']
 })
 export class DiretivasAtributosComponent implements OnInit {
+  //ngClass
   public valor: boolean = true;
+  //ngStyle
   public heightPx: string = "120px";
-  public backgroundColor:string = "#1e1e1e";
-
+  public backgroundColor:string = "#751d1d";
+  public colorFonte: string = "#e1e1e1";
+  public red:number = 0;public green:number = 0;public blue:number = 0;
+  public rgb:string = `rgb(${this.red},${this.green},${this.blue})`;
+  //ngModel
+  public nome:string = "";
+  public list: Array<{nome: string}> = [];
 
   constructor() {
     //
@@ -18,14 +25,26 @@ export class DiretivasAtributosComponent implements OnInit {
   ngOnInit(): void {
     setInterval(()=>{
       if(this.heightPx == "120px"){
-        this.heightPx = "150px";
-        this.backgroundColor = "#e1e1e1";
+        this.heightPx = "121px";
+        this.colorFonte = "#e1e1e1";
+        if(this.red == 255){
+          while(this.blue != 255){
+            this.blue+=5;
+            ++this.green;
+          }
+        }else{
+          this.red+=5;
+        }
+
+        this.backgroundColor = `rgb(${this.red},${this.green},${this.blue})`
       }else{
         this.heightPx = "120px";
         this.backgroundColor = "#751d1d";
+        this.colorFonte = "#1e1e1e";
       }
 
     },1000)
+
   }
 
   public dark(){
@@ -34,5 +53,10 @@ export class DiretivasAtributosComponent implements OnInit {
     }else{
       this.valor = true;
     }
+  }
+
+  //ngModel
+  public salvar(){
+    this.list.push({nome: this.nome});
   }
 }

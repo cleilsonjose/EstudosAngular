b2e0d8b244ba73ad0ec2c14f9b26e3daf07ba85c
cleilsonjose <cleilsonrock@gmail.com>
2022-10-21 11:48:03 -0300

    @input()

--- meu-projeto-1/src/app/app.component.ts
+++ meu-projeto-1/src/app/app.component.ts
@@ -14,11 +14,17 @@ import { Component, OnInit } from '@angular/core';
     </app-diretivas-atributos>
     -->
     <app-new-component></app-new-component>
+    <app-input [contador] = "addValue"></app-input>
+    <button (click)="add()">add</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
+  public addValue: number = 0;
   ngOnInit(): void {
     //
+  }
+  public add(){
+    this.addValue+=1
   }
 }
